38e8e485f428edbff0a250370d3f285e82cd2b6c
Nathaniel Ringo <remexre@gmail.com>
2016-09-19 15:42:47 -0500

    So there've been changes sitting here since Aug 27th...

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e;
 
-go get github.com/project-domino/domino-go/cmd/domino-api \
-	github.com/project-domino/domino-go/cmd/domino-templates;
+go get -v github.com/project-domino/go-domino/cmd/domino-api \
+	github.com/project-domino/go-domino/cmd/domino-templates;
 cp "${GOPATH}/bin/domino-api" api/domino-api;
 cp "${GOPATH}/bin/domino-templates" templates/domino-templates;
 docker-compose build;
